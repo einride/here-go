cfb84d1cb305f7432793917547a00428a48c182a
Alexandre Thenorio <alethenorio@users.noreply.github.com>
2020-07-02 20:42:33 +0200

    feat: add routing v7 bare-bones API
    Add minimal support for routing v7 API

---
+++ pkg/routing/v7/response.go
@@ -0,0 +1,80 @@
+package routingv7
+
+import "encoding/json"
+
+type CalculateRouteResponse struct {
+	MetaInfo RouteResponseMetaInfo `json:"metaInfo,omitempty"`
+	Route    []RouteResponseType   `json:"route,omitempty"`
+}
+
+type RouteResponseMetaInfo struct {
+	// Mirrored RequestId value from the request structure. Used to trace requests.
+	RequestID string `json:"requestId,omitempty"`
+	// Time at which the search was performed.
+	Timestamp string `json:"timestamp,omitempty"`
+	// Gives the version of the underlying map, upon which the route calculations are based.
+	MapVersion string `json:"mapVersion,omitempty"`
+	// Gives a list of map versions, upon which the route calculations are based.
+	AvailableMapVersion []string `json:"availableMapVersion,omitempty"`
+	// Gives the version of the module that performed the route calculations.
+	ModuleVersion string `json:"moduleVersion,omitempty"`
+	// Required. Gives the version of the schema definition to enable
+	InterfaceVersion string `json:"interfaceVersion,omitempty"`
+}
+
+type RouteResponseType struct {
+	// Overall route distance and time summary.
+	Summary RouteResponseSummaryType `json:"summary,omitempty"`
+}
+
+type RouteResponseSummaryType struct {
+	// Indicates total travel distance for the route, in meters.
+	Distance float64 `json:"distance,omitempty"`
+	// Contains the travel time estimate in seconds for this element, considering traffic
+	// and transport mode. Based on the TrafficSpeed. The service may also account for additional
+	// time penalties, so this may be greater than the element length divided by the TrafficSpeed.
+	TrafficTime float64 `json:"traffictime,omitempty"`
+	// Contains the travel time estimate in seconds for this element, considering transport mode but
+	// not traffic conditions. Based on the BaseSpeed. The service may also account for additional
+	// time penalties, therefore this may be greater than the element length divided by the BaseSpeed.
+	BaseTime float64 `json:"baseTime,omitempty"`
+	// Special link characteristics (like ferry or motorway usage) which are covered by the route.
+	Flags []RouteLinkFlag `json:"flags,omitempty"`
+	// Total travel time in seconds optionally considering traffic depending on the request parameters.
+	TravelTime float64 `json:"travelTime,omitempty"`
+	// Textual description of route summary. Supported languages are US and GB English, French, German,
+	// Italian, Polish, Portuguese, Spanish and Traditional Chinese. Element is not provided in case of
+	// other languages.
+	Text string `json:"text,omitempty"`
+	// Estimation of the carbon dioxide emission for the given Route. The value depends on the VehicleType
+	// request parameter which specifies the average fuel consumption per 100 km and the type of combustion
+	// engine (diesel or gasoline). Unit is kilograms with precision to three decimal places.
+	CO2Emission json.RawMessage `json:"co2Emission,omitempty"`
+}
+
+type RouteLinkFlag string
+
+const (
+	// Link can only be traversed by using a boat ferry
+	RouteLinkFlagBoatFerry = "ferry"
+	// Link is part of a dirt road
+	RouteLinkFlagDirtRoad = "dirtRoad"
+	// Link can only be traversed by using high-occupancy vehicle (HOV) lanes
+	RouteLinkFlagHOVLane = "HOVLane"
+	// Link is part of a motorway
+	RouteLinkFlagMotorway = "motorway"
+	//Link is part of a road that you can enter but you have to exit the same way
+	RouteLinkFlagNoThroughRoad = "noThroughRoad"
+	// Link is part of a park
+	RouteLinkFlagPark = "park"
+	// Link is part of a private road
+	RouteLinkFlagPrivateRoad = "privateRoad"
+	// Link can only be traversed by using a rail ferry
+	RouteLinkFlagRailFerry = "railFerry"
+	// Link is part of a toll road
+	RouteLinkFlagTollRoad = "tollRoad"
+	// Link passes through a tunnel
+	RouteLinkFlagTunnel = "tunnel"
+	// Link is part of a built-up area
+	RouteLinkFlagBuiltUpArea = "builtUpArea"
+)
---
+++ pkg/routing/v7/types.go
@@ -0,0 +1,230 @@
+package routingv7
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type WaypointParameter interface {
+	QueryString() string
+}
+
+type WaypointTypeEnum int
+
+const (
+	WaypointTypInvalid WaypointTypeEnum = iota
+	WaypointTypeStopOver
+	WaypointTypePassthrough
+)
+
+type WaypointType struct {
+	// 180 degree turns are allowed for WaypointTypeStopOver but not for WaypointTypePassthrough.
+	// Waypoints defined through a drag-n-drop action should be marked as pass-through.
+	// PassThrough waypoints will not appear in the list of maneuvers.
+	Type WaypointTypeEnum
+	// Stopover delay in seconds. Impacts time-aware calculations. Ignored for WaypointTypePassthrough.
+	StopOverDuration int
+}
+
+func (t WaypointType) String() string {
+	var out string
+	switch t.Type {
+	case WaypointTypePassthrough:
+		out = "passThrough!"
+	case WaypointTypeStopOver:
+		out = "stopOver"
+		if t.StopOverDuration > 0 {
+			out += ","
+			out += strconv.Itoa(t.StopOverDuration)
+		}
+		out += "!"
+	}
+	return out
+}
+
+type GeoWaypoint struct {
+	Lat          float64
+	Long         float64
+	WaypointType WaypointType
+	// In meters
+	Altitude int32
+	// Matching Links are selected within the specified TransitRadius.
+	// For example to drive past a city without necessarily going into the city center you can
+	// specify the coordinates of the center and a TransitRadius of 5000m.
+	// In meters
+	TransitRadius int
+	// Custom label identifying this waypoint.
+	UserLabel string
+	// Heading (at the startpoint), in degrees starting at true north and continuing clockwise around the compass.
+	// North is 0 degrees, East is 90 degrees, South is 180 degrees, and West is 270 degrees.
+	// MUST be in range [0,360]
+	Heading float32
+}
+
+func (w *GeoWaypoint) QueryString() string {
+	builder := strings.Builder{}
+	builder.WriteString("geo!")
+	if wt := w.WaypointType.String(); wt != "" {
+		builder.WriteString(wt)
+	}
+	builder.WriteString(strconv.FormatFloat(w.Lat, 'f', 8, 64))
+	builder.WriteString(",")
+	builder.WriteString(strconv.FormatFloat(w.Long, 'f', 8, 64))
+	if w.TransitRadius > 0 {
+		builder.WriteString(";")
+		builder.WriteString(strconv.Itoa(w.TransitRadius))
+	}
+	if w.UserLabel != "" {
+		builder.WriteString(";")
+		builder.WriteString(w.UserLabel)
+	}
+	if w.Heading > 0 {
+		builder.WriteString(";")
+		builder.WriteString(fmt.Sprintf("%f", w.Heading))
+	}
+	return builder.String()
+}
+
+type RoutingMode struct {
+	// Type provides identifiers for different optimizations which can be applied during the route calculation.
+	// Selecting the routing type affects which constraints, speed attributes and weights
+	// are taken into account during route calculation.
+	// Defaults to RouteTypeFastest.
+	Type RouteType
+	// Depending on the transport mode special constraints, speed attributes and weights are taken into account during
+	// route calculation.
+	// Defaults to TransportModeCar.
+	TransportMode TransportModeType
+	// Specify whether to optimize a route for traffic.
+	// Note: Difference between traffic disabled and enabled affects only the calculation of the route.
+	// Traffic time of the route will still be calculated for all routes using the same rules as for traffic:enabled.
+	// Optional.
+	TrafficMode TrafficModeType
+	// TODO: Implement RouteFeature
+	// https://developer.here.com/cn/documentation/routing/topics/resource-param-type-routing-mode.html#type-route-feature
+}
+
+func (m RoutingMode) String() string {
+	var out string
+	out += m.Type.String()
+	out += ";"
+	out += m.TransportMode.String()
+	out += ";"
+	if m.TrafficMode != TrafficModeTypeInvalid {
+		out += "traffic:"
+		out += m.TrafficMode.String()
+		out += ";"
+	}
+	return out
+}
+
+type RouteType int
+
+const (
+	// Route calculation from start to destination optimized by travel time.
+	// Depending on the traffic mode provided by the request, travel time is determined with or without
+	// traffic information.
+	// In some cases, the route returned by the fastest mode may not be the route with the shortest possible
+	// travel time.
+	// For example, the routing service may favor a route that remains on a highway, even if a shorter travel
+	// time can be achieved by taking a detour or shortcut through a side road.
+	RouteTypeFastest RouteType = iota
+	// Route calculation from start to destination disregarding any speed information. In this mode, the distance of the
+	// route is minimized, while keeping the route sensible.
+	// This includes, for example, penalizing turns. Because of that, the resulting route will not necessarily
+	// be the one with minimal distance.
+	RouteTypeShortest
+	// Route calculation from start to destination optimizing based on combination of travel time and distance.
+	RouteTypeBalanced
+)
+
+func (t RouteType) String() string {
+	var out string
+	switch t {
+	case RouteTypeFastest:
+		out = "fastest"
+	case RouteTypeShortest:
+		out = "shortest"
+	case RouteTypeBalanced:
+		out = "balanced"
+	}
+	return out
+}
+
+type TransportModeType int
+
+const (
+	// Route calculation for cars
+	TransportModeCar TransportModeType = iota
+	// Route calculation for a pedestrian. As one effect, maneuvers will be optimized for walking,
+	// that is segments will consider actions relevant for pedestrians and maneuver instructions will
+	// contain texts suitable for a walking person. This mode disregards any traffic information.
+	TransportModePedestrian
+	// Route calculation for HOV (high-occupancy vehicle) cars
+	TransportModeCarHOV
+	// Route calculation for bicycles. This mode uses the pedestrian road network, but uses different
+	// speeds based on each road's suitability for cycling. Pedestrian roads that are also open for cars
+	// in the travel direction are considered open for cycling, as are pedestrian roads located in parks.
+	// These roads use full bicycle speed for routing. Other pedestrian roads, including travelling the wrong
+	// way down one-way streets, are considered at the pedestrian walking speed, as it is assumed the bicycle
+	// must be pushed.
+	TransportModeBicycle
+)
+
+func (t TransportModeType) String() string {
+	var out string
+	switch t {
+	case TransportModeCar:
+		out = "car"
+	case TransportModePedestrian:
+		out = "pedestrian"
+	case TransportModeCarHOV:
+		out = "carHOV"
+	case TransportModeBicycle:
+		out = "bicycle"
+	}
+	return out
+}
+
+type TrafficModeType int
+
+const (
+	TrafficModeTypeInvalid TrafficModeType = iota
+	// No departure time provided: This behavior is deprecated and will return error in the future.
+	// 	* Static time based restrictions: Ignored
+	// 	* Real-time traffic closures: Valid for entire length of route.
+	// 	* Real-time traffic flow events: Speed at calculation time used for entire length of route.
+	//
+	// Departure time provided:
+	// 	* Static time based restrictions: Avoided if road would be traversed within validity period of the restriction.
+	// 	* Real-time traffic closures: Avoided if road would be traversed within validity period of the incident.
+	// 	* Real-time traffic flow events: Speed used if road would be traversed within validity period of the flow event.
+	TrafficModeTypeEnabled
+	// No departure time provided:
+	// 	* Static time based restrictions: Ignored
+	// 	* Real-time traffic closures: Ignored.
+	// 	* Real-time traffic flow speed: Ignored.
+	//
+	// Departure time provided:
+	// 	* Static time based restrictions: Avoided if road would be traversed within validity period of the restriction.
+	// 	* Real-time traffic closures: Valid for entire length of route.
+	// 	* Real-time traffic flow speed: Ignored.
+	TrafficModeTypeDisabled
+	// Let the service automatically apply traffic related constraints that are suitable for the selected routing type,
+	// transport mode, and departure time. Also user entitlements will be taken into consideration.
+	TrafficModeTypeDefault
+)
+
+func (t TrafficModeType) String() string {
+	var out string
+	switch t {
+	case TrafficModeTypeEnabled:
+		out = "enabled"
+	case TrafficModeTypeDisabled:
+		out = "disabled"
+	case TrafficModeTypeDefault:
+		out = "default"
+	}
+	return out
+}
