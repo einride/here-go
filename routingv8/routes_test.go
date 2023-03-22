a6c55eb689cf394c3d5f5cf5a367da37cf29515f
Alfred Gunnargård <alfred.gunnargard@einride.tech>
2023-03-22 08:13:42 +0100

    feat: expose ResponseError and include HTTPBody as string
    We've seen cases of empty HereErrorResponse structs. So the parsing seems to not always work.
    In order to enable improved logging, we add the HTTPBody as a string in the newly exposed ResponseError struct.

--- routingv8/routes_test.go
+++ routingv8/routes_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"testing"
@@ -15,6 +16,7 @@ import (
 type RoutesMock struct {
 	responseStatus int
 	responseBody   routingv8.RoutesResponse
+	error          *routingv8.HereErrorResponse
 }
 
 func (c *RoutesMock) Do(req *http.Request) (*http.Response, error) {
@@ -23,6 +25,12 @@ func (c *RoutesMock) Do(req *http.Request) (*http.Response, error) {
 	b, err := json.Marshal(c.responseBody)
 	if err != nil {
 		return nil, err
+	}
+	if c.error != nil {
+		b, err = json.Marshal(c.error)
+		if err != nil {
+			return nil, err
+		}
 	}
 	r := bytes.NewReader(b)
 	return &http.Response{
@@ -87,4 +95,38 @@ func TestRoutingervice_Routes(t *testing.T) {
 	})
 	assert.NilError(t, err)
 	assert.DeepEqual(t, &exp, got)
+}
+
+func TestRoutingervice_Routes_Error(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	// Einride Gothenburg.
+	origin := routingv8.GeoWaypoint{
+		Lat:  57.707752,
+		Long: 11.949767,
+	}
+	// Einride Stockholm.
+	destination := routingv8.GeoWaypoint{
+		Lat:  59.337492,
+		Long: 18.063672,
+	}
+
+	exp := routingv8.HereErrorResponse{
+		Title:  "Mocked Error",
+		Status: 400,
+	}
+
+	httpClient := RoutesMock{responseStatus: 400, error: &exp}
+	routingClient := routingv8.NewClient(&httpClient)
+
+	_, err := routingClient.Routing.Routes(ctx, &routingv8.RoutesRequest{
+		Origin:        origin,
+		Destination:   destination,
+		TransportMode: routingv8.TransportModeCar,
+	})
+	var responseError *routingv8.ResponseError
+	assert.Check(t, errors.As(err, &responseError))
+	assert.DeepEqual(t, responseError.Response, &exp)
+	assert.Check(t, responseError.HTTPBody != "")
 }
